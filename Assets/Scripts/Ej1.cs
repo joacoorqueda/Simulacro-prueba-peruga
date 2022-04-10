f4cf1a6c5a4c54e0c4679d2a459bb8742c90afce
joacoorqueda <101809787+joacoorqueda@users.noreply.github.com>
2022-04-10 15:07:25 -0300

    Initial commit

---
+++ Assets/Scripts/Ej1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+public class Ej1 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("Los numeros son iguales");
+        }
+
+        else if (num2 == num1)
+        {
+            Debug.Log("Los numeros son iguales");
+        }
+
+        else
+        {
+            Debug.Log("Los numeros no son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ej2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+////Crear las variables necesarias para /*ingresar por Inspector el precio de 3 productos y un monto de dinero disponible.Comunicar si la suma de los mismos supera o no el monto de dinero disponible
+//.Luego indicar cuánto dinero sobra o falta.*/
+public class Ej2 : MonoBehaviour
+{
+    public int producto1;
+    public int producto2;
+    public int producto3;
+    public int montodinero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int productos = producto1 + producto2 + producto3;
+
+        if (productos > montodinero)
+
+        {
+            Debug.Log("La suma de los productos excede tu monto de dinero");
+            Debug.Log("Te falta" + (productos - montodinero));
+        }
+
+        else
+        {
+            Debug.Log("La suma de los productos no excede tu monto de dinero");
+            Debug.Log("Te sobra " + (montodinero - productos));
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
